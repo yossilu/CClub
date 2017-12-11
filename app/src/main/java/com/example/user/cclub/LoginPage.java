45f0956a8e345116e043656ca8bf680582fc7595
Yossi Lufton <yossil153@gmail.com>
2017-12-11 21:21:26 +0200

    Initial commit

---
+++ app/src/main/java/com/example/user/cclub/LoginPage.java
@@ -0,0 +1,12 @@
+package com.example.user.cclub;
+
+import android.support.annotation.NonNull;
+import android.support.design.internal.NavigationMenuItemView;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class LoginPage extends AppCompatActivity {
+
+}
---
+++ app/src/main/java/com/example/user/cclub/MenuFooter.java
@@ -0,0 +1,9 @@
+package com.example.user.cclub;
+
+/**
+ * Created by user on 12/11/2017.
+ */
+
+public class MenuFooter {
+
+}
