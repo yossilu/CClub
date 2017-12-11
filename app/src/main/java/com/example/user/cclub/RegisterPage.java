74c59adc2546d56ac806a6b2e62838fef3b8a8a4
Yossi Lufton <yossil153@gmail.com>
2017-12-12 01:11:56 +0200

    comm

--- app/src/main/java/com/example/user/cclub/RegisterPage.java
+++ app/src/main/java/com/example/user/cclub/RegisterPage.java
@@ -1,6 +1,9 @@
 package com.example.user.cclub;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
@@ -8,19 +11,52 @@ import android.widget.ImageView;
 
 
 public class RegisterPage extends AppCompatActivity implements View.OnClickListener {
-    ImageView imageToUpload;
-    ImageButton imgBtn;
+
+    private static final int RESULT_LOAD_IMG = 1;
+    CameraPhoto cameraPhoto;
+    ImageView imageToUpload , imagePreview;
+    ImageButton cameraBtn;
+    final int CAMERA_REQUEST = 13323;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         imageToUpload = (ImageView)findViewById(R.id.imageViewInfo);
-        imgBtn = (ImageButton) findViewById(R.id.imageButtonReg);
+        imagePreview = (ImageView)findViewById(R.id.imageViewInfo);
+        cameraBtn = (ImageButton) findViewById(R.id.imageButtonReg);
+        cameraPhoto = new CameraPhoto(getApplicationContext());
+
+        imageToUpload.setOnClickListener(this);
+        cameraBtn.setOnClickListener(this);
     }
 
 
     @Override
-    public void onClick(View v) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if((requestCode == RESULT_LOAD_IMG) && (resultCode == RESULT_OK) && (data != null)){
+            Uri selectImg = data.getData();
+            imageToUpload.setImageURI(selectImg);
+        }
+        if(resultCode == RESULT_OK){
+            if(resultCode == CAMERA_REQUEST){
+                cameraPhoto.getPhotoPath();
+            }
+        }
+    }
 
+    @Override
+    public void onClick(View v) {
+        Intent gallery;
+        switch (v.getId()) {
+            case R.id.imageViewInfo:
+                gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(gallery,RESULT_LOAD_IMG);
+                break;
+            case R.id.imageButtonReg:
+                startActivityForResult(cameraPhoto.takePhotoIntent(),CAMERA_REQUEST);
+                cameraPhoto.addToGallery();
+                break;
+        }
     }
 }
